a89c70cc06ed94e3dd540ba677b5208a0aa3b4a3
Sukhatai09 <65112429@dpu.ac.th>
2025-04-02 11:13:05 +0700

    feat: add custom font family configuration and remove unused font file

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,6 +1,13 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 import "./global.css";
 export default function RootLayout() {
+  useFonts({
+    prompt: require("../assets/fonts/Prompt-Light.ttf"),
+    "prompt-bold": require("..//assets/fonts/Prompt-Bold.ttf"),
+    "prompt-regular": require("../assets/fonts/Prompt-Medium.ttf"),
+    
+  });
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Prompt: ["Prompt", "sans-serif"],
+        PromptMedium: ["Prompt-Medium", "sans-serif"],
+        PromptBold: ["Prompt-Bold", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
