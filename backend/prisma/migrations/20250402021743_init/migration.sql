8bc44547912b9c52c0c8f4c1b8cbc659b5a279e8
Sukhatai09 <65112429@dpu.ac.th>
2025-04-04 09:56:50 +0700

    feat: implement authentication service and routes, update database schema for user management

--- backend/prisma/migrations/20250402021743_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Cream" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Cream_pkey" PRIMARY KEY ("id")
-);
--- backend/src/routes/flowerRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const prisma = require("../prismaClient");
-const router = express.Router();
-
-const  flowerController = require("../controller/flowerController");
-
-router.post("/cream",flowerController.createFlower);
-
-module.exports = router;
