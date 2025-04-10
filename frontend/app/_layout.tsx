21891f49485bb7937da0db5fa4a00c033c66af2f
Sukhatai09 <65112429@dpu.ac.th>
2025-04-10 16:35:35 +0700

    update

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,12 +3,14 @@ import { useMemo } from "react";
 
 export default function Layout() {
   const pathname = usePathname();
-  
+
   const showNavbar = useMemo(() => pathname !== "/", [pathname]);
 
-  return <Stack screenOptions={{ headerShown: !showNavbar }}>
-    <Stack.Screen name="index" />
-    <Stack.Screen name="(tabs)" />
-    <Stack.Screen name="(screen)" />
-  </Stack>;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(screen)" />
+    </Stack>
+  );
 }
