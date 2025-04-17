57482c43bf450d89e7d1c456e675aadab790f0d8
Sukhatai09 <65112429@dpu.ac.th>
2025-04-17 14:43:22 +0700

    update

--- backend/src/main.js
+++ backend/src/main.js
@@ -11,6 +11,7 @@ import userRoutes from "./routes/userRoutes.js";
 import orderItemRoutes from "./routes/orderItemRoutes.js";
 import flowerLikeRoutes from "./routes/flowerLikeRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
+import cartItemRoutes from "./routes/cartItemRoutes.js";
 
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
@@ -31,6 +32,7 @@ app.use('/api',userRoutes);
 app.use('/api',orderItemRoutes);
 app.use('/api',flowerLikeRoutes);
 app.use('/api',cartRoutes);
+app.use('/api',cartItemRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
