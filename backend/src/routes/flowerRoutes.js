3148a22f59b2f044e68abb70f88e66e903cb5cec
Sukhatai09 <65112429@dpu.ac.th>
2025-04-21 18:53:28 +0700

    Remove adminOnly middleware from flower routes and update API_URL in app.json

--- backend/src/routes/flowerRoutes.js
+++ backend/src/routes/flowerRoutes.js
@@ -7,7 +7,7 @@ import adminOnly from "../middleware/adminOnly.js";
 const router = express.Router();
 router.get("/flower/:id",getFlowerById); //ดึงข้อมูลทั้งหมด
 router.get("/flower",getAllFlower); //ดึงข้อมูลทั้งหมด
-router.post("/flower",adminOnly, upload.single("image_url"), createFlower); //ใช้ multer ในการอัพโหลดไฟล์ภาพ
-router.put("/flower/:id",adminOnly, upload.single("image_url"), updateFlower); //ใช้ multer ในการอัพโหลดไฟล์ภาพ
-router.delete("/flower/:id",adminOnly,deleteFlower); //ใช้ multer ในการอัพโหลดไฟล์ภาพ
+router.post("/flower", upload.single("image_url"), createFlower); //ใช้ multer ในการอัพโหลดไฟล์ภาพ
+router.put("/flower/:id", upload.single("image_url"), updateFlower); //ใช้ multer ในการอัพโหลดไฟล์ภาพ
+router.delete("/flower/:id",deleteFlower); //ใช้ multer ในการอัพโหลดไฟล์ภาพ
 export default router;
