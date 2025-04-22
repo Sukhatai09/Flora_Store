fc270bc4cc7cb6a4565bb945692eaa5be4d6b58b
Sukhatai09 <65112429@dpu.ac.th>
2025-04-22 18:39:16 +0700

    updat ดึงชื่อ admin  มาโชว์แถบบน admin

--- admin/src/pages/Layout.tsx
+++ admin/src/pages/Layout.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
 import topLogo from "../assets/Flora-logo.png";
+import useAuthStore from "../store/authStore";
 
 const Layout: React.FC = () => {
+  const user = useAuthStore((state) => state.user);
   return (
     <div>
       <div>
         <div className="bg-[#F8DAE2] w-full h-[70px] flex justify-center items-center fixed top-0 z-10">
           <img src={topLogo} alt="Logo" className="h-full w-auto" />
           <h1 className="ml-3 text-2xl text-[#ed51b1] font-bold ">FLORA</h1>
+          <h1 className="ml-3 text-1xl text-[#ed51b1] font-bold absolute right-5">ADMIN : {user?.first_name.toUpperCase()}</h1>
         </div>
         <div className="fixed top-[70px] left-0 z-20">
           <Sidebar />
